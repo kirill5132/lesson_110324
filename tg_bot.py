b75570f08d847f67b003950e89974433cf470566
sn002 <sn0020068@gmail.com>
2024-03-22 21:19:03 +0300

    home

--- tg_bot.py
+++ tg_bot.py
@@ -1,26 +1,8 @@
-import pprint
 import telebot
-import requests
 import random
 
-bot = telebot.TeleBot('token1')
-
-token1 = '7037321686:AAFBRtsXC2T7dDuWj4okyhtq2j7ziQHbfoc'
-main_url = f'https://api.telegram.org/bot{token1}'
-
-url = f'{main_url}/getUpdates'
-result = requests.get(url)
-pprint.pprint(result.json())
-
-messages = result.json()['result']
-for message in messages:
-    chat_id = message['message']['chat']['id']
-    url = f'{main_url}/sendMessage'
-    params = {
-        'chat_id': chat_id,
-        'text': 'Привет дорогой пользователь'
-    }
-    result = requests.post(url, params=params)
+token = '7037321686:AAFBRtsXC2T7dDuWj4okyhtq2j7ziQHbfoc'
+bot = telebot.TeleBot(token)
 
 
 @bot.message_handler(commands=['quote'])
@@ -28,7 +10,7 @@ def send_welcome(message1):
     bot.reply_to(message1, 'у сигма сигма')
 
 
-@bot.message_handler(content_types=['sigma'])
+@bot.message_handler(commands=['sigma'])
 def send_sticker(message1):
     print(message1)
     file_id = 'CAACAgEAAxkBAAIBa2X0Zy6kg0p_iIoyXyy5kexA5VmmAAJ-AQACDmEBRASfnE_40iadNAQ'
@@ -40,10 +22,12 @@ def send_random():
     numbers = random.randint(1, 100)
     print(numbers)
 
+
 @bot.message_handler(commands=['audio'])
-def send_audio():
-    audio = open("argenby-sigma-iz-tik-toka.mp3", 'rb')
-    bot.send_audio(message.chat.id, "AgADIksAArST8Es")
+def send_audio(message1):
+    print(message1)
+    open("argenby-sigma-iz-tik-toka.mp3", 'rb')
+    bot.send_audio(message1.chat.id, "AgADIksAArST8Es")
 
 
 bot.polling()
